c9cd8c4ffb155e999d25843a33517f996b4cf9fc
Rackover <rackover@racknet.noip.me>
2020-03-20 23:52:57 +0100

    Added file browser buttons

--- public/res/js/fileExplorer.js
+++ public/res/js/fileExplorer.js
@@ -3,10 +3,15 @@ $( document ).ready(init)
 let display;
 let currentPathDisplay;
 let baseEntries = []
+let selectedDiv = null;
 
 function init(){
     display = $("#folderView");
     currentPathDisplay = $("#currentPath")
+    display.click(function(e){
+        e.stopPropagation()
+        select()
+    })
 }
 
 function loadEntries(path, entries, areBaseEntries = false){
@@ -23,24 +28,44 @@ function appendEntryDiv(entry){
     const entryDiv = document.createElement("div")
     entryDiv.className = "entry"
     if (entry.type == "dir"){
-        entryDiv.addEventListener("click", function(){explorePath(entry)})
+        entryDiv.addEventListener("click", function(e){
+            e.stopPropagation()
+            if (selectedDiv == entryDiv){
+                explorePath(entry)
+            }
+            else{
+                select(entryDiv)
+            }
+        })
     }
     else if (entry.type == "previous"){
-        entryDiv.addEventListener("click", function(){
-            let parts = entry.path.split("/")
-            let entries = baseEntries
-            for(let i = 0; i < parts.length-1; i++){
-                for(j in entries){
-                    if (entries[j].name == parts[i]){
-                        const ntr = entries[j]
-                        entries = ntr.children
-                        break
+        entryDiv.addEventListener("click", function(e){
+            e.stopPropagation()
+            if (selectedDiv == entryDiv){
+                let parts = entry.path.split("/")
+                let entries = baseEntries
+                for(let i = 0; i < parts.length-1; i++){
+                    for(j in entries){
+                        if (entries[j].name == parts[i]){
+                            const ntr = entries[j]
+                            entries = ntr.children
+                            break
+                        }
                     }
                 }
-            }
-            parts.pop()
+                parts.pop()
 
-            loadEntries(parts.join("/"), entries)
+                loadEntries(parts.join("/"), entries)
+            }
+            else{
+                select(entryDiv)
+            }
+        })
+    }
+    else if (entry.type == "file"){
+        entryDiv.addEventListener("click", function(e){
+            e.stopPropagation()
+            select(entryDiv)
         })
     }
     const preview = document.createElement("div")
@@ -73,4 +98,19 @@ function appendEntryDiv(entry){
 
 function explorePath(entry){
     loadEntries(entry.path, entry.children)
+}
+
+function select(div=null){
+    if (selectedDiv != null){
+        selectedDiv.className = selectedDiv.className.replace(" selected", "");
+    }
+    if (div == null){
+        $("#destroy").addClass("disabled");
+        return;
+    } 
+
+    $("#destroy").removeClass("disabled");
+    div.className += " selected";
+    selectedDiv = div;
+
 }
